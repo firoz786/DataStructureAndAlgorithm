1fb3a14f4df1354c1eb92e00559016f73255808e
firoz alam <firozalam.786123@gmail.com>
2023-02-11 16:50:37 +0530

    Add : contain duplicated

--- DataStructure&Algorithm/VidyaSagarDSA/vidyaSagarGreen/ContainsDuplicate.java
+++ DataStructure&Algorithm/VidyaSagarDSA/vidyaSagarGreen/ContainsDuplicate.java
@@ -1,5 +1,24 @@
 package vidyaSagarGreen;
 
-public class ContainsDuplicate {
+import java.util.HashSet;
+import java.util.Set;
 
+public class ContainsDuplicate {
+	public static boolean containsDuplicate(int[] nums) {
+		boolean status = false;
+
+		Set<Integer> set = new HashSet<>();
+		for (Integer integer : nums) {
+			if (set.add(integer) == false)
+				return true;
+		}
+
+		return status;
+	}
+
+	public static void main(String[] args) {
+		int[] num = { 1, 23, 4 };
+		containsDuplicate(num);
+
+	}
 }
